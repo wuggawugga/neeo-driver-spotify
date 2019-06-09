5c080a63fc3d074aa07175612bf474955773e2cd
wuggawugga <wuggawugga@users.noreply.github.com>
2019-06-09 20:59:44 +0200

    Added HTML form to handle credentials

--- config.js
+++ config.js
@@ -32,8 +32,6 @@ module.exports = {
 		{key: 'root.browse.charts.denmark_viral_50', id: '37i9dQZEVXbMA8BIYDeMkD'},
 	],
 	auth: {
-		client_id: '1045e18758344e7cb73a06b08863f82b',
-		client_secret: 'fcd47558e83745298fdba2fbb24e1211',
 		scopes: ['user-read-recently-played', 'user-top-read', 'user-library-read', 'playlist-read-private', 'playlist-read-collaborative', 'user-read-email', 'user-read-birthdate', 'user-read-private', 'user-read-playback-state', 'user-modify-playback-state', 'user-read-currently-playing', 'app-remote-control', 'streaming', 'user-follow-read'],
 		state_salt: 'salty brine',
 		redirect_url: 'http://localhost:3042/authorize',
